abf525a7bc32f0799061f60e6ef977ff8e325e12
Marcel <ikbenmarcelpos@gmail.com>
2020-01-07 12:24:25 +0100

    🔨 Committing some default components and sass

--- src/App.svelte
+++ src/App.svelte
@@ -1,27 +1,82 @@
 <h1>
   Hello my name is
-  <span class="name">{data.name}</span>
+  <span class="{$store.name ? 'name' : ''}">{$store.name ? $store.name : 'undefined'}</span>
   and I am
-  <span class="age">{data.age}</span>
+  <span class="{$store.age ? 'age' : ''}">{$store.age}</span>
   years old.
 </h1>
 
-<button on:click="{handleBabyMe}">Baby me</button>
-<button on:click="{handleAgeMe}">Age me</button>
+<textarea bind:value="{html}"></textarea>
+<button on:click="{handlePublishHtml}">Publish</button>
+
+<div class="html-preview">
+  <small class="html-preview__title">HTML Preview</small>
+
+  <div class="html-preview__content">
+    {#if html}
+      {@html html}
+    {:else}
+      <span class="error">No html entered</span>
+    {/if}
+  </div>
+</div>
+
+<div class="html-published">
+  {#each publishedHtml as html, i (html.id)}
+    <div>
+      <h2>#{i}</h2>
+      <small>{html.id}</small>
+      <div>
+        {@html html.content}
+      </div>
+    </div>
+  {/each}
+</div>
+
+<!-- <Form
+  on:update-name="{e => ($store.name = e.detail)}"
+  on:update-age="{e => ($store.age = e.detail)}"
+  {$store.name}
+  {$store.age}
+/> -->
+
+<Form />
 
 <script>
-  const data = {
-    name: 'Marcel',
-    age: 29,
+  import { createEventDispatcher, onMount, onDestroy, beforeUpdate, afterUpdate } from 'svelte';
+  import uuid from 'uuid/v4';
+
+  import Form from '@/components/Form';
+  import store from '@/store/index';
+
+  let html = 'This is <strong>strong</strong> html boi!';
+  let publishedHtml = [];
+
+  // Watcher variable
+  // $: bigName = name ? name.toUpperCase() : 'Name unknown';
+
+  const handlePublishHtml = () => {
+    publishedHtml = [{ id: uuid(), content: html }, ...publishedHtml];
+    html = '';
   };
 
-  const handleBabyMe = () => {
-    data.age -= 1;
-  };
+  beforeUpdate(() => {
+    console.log('before update');
+  });
 
-  const handleAgeMe = () => {
-    data.age += 1;
-  };
+  afterUpdate(() => {
+    console.log('after update');
+  })
+
+  onMount(() => {
+    console.log('mounted');
+  });
+
+  onDestroy(() => {
+    console.log('destroyed');
+  });
 </script>
 
-<style src="./App.scss" lang="scss"></style>
+<style src="./App.scss" lang="scss">
+
+</style>
---
+++ src/components/Form.svelte
@@ -0,0 +1,41 @@
+<input type="text" value="{$store.name}" on:input="{handleNameChange}" />
+
+<button on:click="{handleBabyMe}">Baby me</button>
+<button on:click="{handleAgeMe}">Age me</button>
+
+<script>
+import store from '@/store/index';
+// import { createEventDispatcher } from 'svelte';
+
+// const dispatch = createEventDispatcher();
+
+// export let name = ''; // optional prop
+// export let age; // required prop
+
+const handleNameChange = e => {
+  // name = e.target.value;
+  // dispatch('update-name', name);
+  store.update(store => {
+    store.name = e.target.value;
+    return store;
+  });
+}
+
+const handleBabyMe = () => {
+  // age.set -= 1;
+  // dispatch('update-age', age);
+  store.update(store => {
+    store.age = store.age - 1;
+    return store;
+  });
+};
+
+const handleAgeMe = () => {
+  // age.set += 1;
+  // dispatch('update-age', age);
+  store.update(store => {
+    store.age = store.age += 1;
+    return store;
+  });
+};
+</script>
